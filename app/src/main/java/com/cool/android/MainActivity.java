44d550cb01888c88496bf0ff8112c49092d7841e
chao <liuchao6@oppo.com>
2022-06-21 15:02:16 +0800

    change

--- app/src/main/java/com/cool/android/MainActivity.java
+++ app/src/main/java/com/cool/android/MainActivity.java
@@ -3,6 +3,9 @@ package com.cool.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text=(TextView) findViewById(R.id.text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
